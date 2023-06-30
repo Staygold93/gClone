ff5d106f9e54cd225656db6a36b85cd8b0613d74
Pharez <Staygold93@github.com>
2023-06-30 16:52:15 -0500

    added comps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useState } from 'react';
+
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Routes } from './components/Routes';
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
     <div>
         
